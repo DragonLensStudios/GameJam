c9943f17f0e570d93294be0bebe08c98c1c1b051
Autumnos <Autumnos@Dragonlensstudios.com>
2015-10-18 01:43:17 -0600

    added monitor and object interaction scripts
    added monitor and player prefabs, and added object interaction script
    system.

---
+++ Title Goes Here/Assets/Game/Scripts/Object Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	
+	public void LogToConsole(string _logMessage)
+	{
+		Debug.Log (_logMessage);
+	}
+
+	public void MovePlayer(Transform _playerPos)
+	{
+		_playerPos.position += Vector3.up;
+	}
+		
+	public void KillPlayer(GameObject _player)
+	{
+		Destroy (_player);
+	}
+
+
+}

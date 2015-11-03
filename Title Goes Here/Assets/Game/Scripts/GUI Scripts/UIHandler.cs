4fe834ddd46f5b13a0d31560de53559b693bf1e0
Jeffery Miller <Mony@dragonlensstudios.com>
2015-11-03 06:54:38 -0700

    Added ShowMessage Event and Updated Scripts
    Added a Show Message event and some test calls to validate that it is
    working correctly and added a new UI panel and some other stuff and some
    minor changes.

--- Title Goes Here/Assets/Game/Scripts/GUI Scripts/UIHandler.cs
+++ Title Goes Here/Assets/Game/Scripts/GUI Scripts/UIHandler.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UIHandler : MonoBehaviour
@@ -11,6 +12,22 @@ public class UIHandler : MonoBehaviour
     public GameObject ConclusionScreen;
     public Text GameTimeClockText;
     public Text FatigueText;
+    public GameObject MessageBoxGameObject;
+    public Text MessageBoxText;
+
+    private float MessageWaitTime = 1.5f;
+    private bool FadeMessage;
+    
+
+    public void OnEnable()
+    {
+        GameEvents.ShowMessageEvent += ShowMessage;
+    }
+
+    public void OnDisable()
+    {
+        GameEvents.ShowMessageEvent -= ShowMessage;
+    }
 
     public void ToggleGameobject(GameObject _gameObject)
     {
@@ -19,14 +36,44 @@ public class UIHandler : MonoBehaviour
 
     public void Update()
     {
+        CanvasGroup canvas = MessageBoxGameObject.GetComponent<CanvasGroup>();
+
         if (GameTimeClockText != null)
         {
             GameTimeClockText.text = "Time Remaining \n" + " Days " + Game.Manage.GameTimeLimit.Day + " Time: " + Game.Manage.GameTimeLimit.TimeString_H_MM;
+        }
+
+        if(FadeMessage && (int)canvas.alpha == 0)
+        {
+            canvas.alpha = canvas.alpha += Time.deltaTime * MessageWaitTime;
+        }
+
+        if (FadeMessage == false && canvas.alpha > 0)
+        {
+            canvas.alpha = canvas.alpha -= Time.deltaTime * MessageWaitTime;
         }
     }
 
     public void CallStartGameEvent()
     {
-        EventManager.ResetGame();
+        GameEvents.ResetGame();
+        Game.Manage.GameName = GameNameInput.text;
+    }
+
+    public void ShowMessage(string _message, float _time)
+    {
+        if (_time != 1.5f)
+        {
+            MessageWaitTime = _time;
+        }
+        MessageBoxText.text = _message;
+        StartCoroutine(IE_ShowMessageWait(_time));
+    }
+
+    public IEnumerator IE_ShowMessageWait(float _time)
+    {
+        FadeMessage = true;
+        yield return new WaitForSeconds(_time);
+        FadeMessage = false;
     }
 }

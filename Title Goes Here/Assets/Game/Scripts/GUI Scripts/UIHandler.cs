7d8626460a147d4622c7b8ab97c2861f0414589d
Jeffery Miller <mony@dragonlenstudios.com>
2015-11-09 00:43:57 -0700

    UI Handler Refactor
    UI handler refactored.

--- Title Goes Here/Assets/Game/Scripts/GUI Scripts/UIHandler.cs
+++ Title Goes Here/Assets/Game/Scripts/GUI Scripts/UIHandler.cs
@@ -1,79 +1,99 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class UIHandler : MonoBehaviour
+﻿namespace DLS.Games.TitleGoesHere
 {
-    public InputField GameNameInput;
-    public GameObject CurrentAppPanel;
-    public GameObject MainMenuPanel;
-    public GameObject DoorOSMainPanel;
-    public GameObject DoorAppsPanel;
-    public GameObject ConclusionScreen;
-    public Text GameTimeClockText;
-    public Text FatigueText;
-    public GameObject MessageBoxGameObject;
-    public Text MessageBoxText;
-
-    private float MessageWaitTime = 1.5f;
-    private bool FadeMessage;
-    
-
-    public void OnEnable()
+    using System.Collections;
+    using UnityEngine;
+    using UnityEngine.UI;
+    /// <summary>
+    /// This class handles all UI calls and stores UI elements to be used and referenced.
+    /// </summary>
+    public class UIHandler : MonoBehaviour
     {
-        GameEvents.ShowMessageEvent += ShowMessage;
-    }
+        public InputField GameNameInput;
+        public GameObject CurrentAppPanel;
+        public GameObject MainMenuPanel;
+        public GameObject DoorOSMainPanel;
+        public GameObject DoorAppsPanel;
+        public GameObject ConclusionScreen;
+        public Text GameTimeClockText;
+        public Text FatigueText;
+        public GameObject MessageBoxGameObject;
+        public Text MessageBoxText;
 
-    public void OnDisable()
-    {
-        GameEvents.ShowMessageEvent -= ShowMessage;
-    }
+        public GameObject ConclusionPanelPrefab;
 
-    public void ToggleGameobject(GameObject _gameObject)
-    {
-        _gameObject.SetActive(!_gameObject.activeSelf);
-    }
+        private float MessageWaitTime = 1.5f;
+        private bool FadeMessage;
 
-    public void Update()
-    {
-        CanvasGroup canvas = MessageBoxGameObject.GetComponent<CanvasGroup>();
 
-        if (GameTimeClockText != null)
+        public void OnEnable()
         {
-            GameTimeClockText.text = "Time Remaining \n" + " Days " + Game.Manage.GameTimeLimit.Day + " Time: " + Game.Manage.GameTimeLimit.TimeString_H_MM;
+            GameEvents.ShowMessageEvent += ShowMessage;
         }
 
-        if(FadeMessage && (int)canvas.alpha == 0)
+        public void OnDisable()
         {
-            canvas.alpha = canvas.alpha += Time.deltaTime * MessageWaitTime;
+            GameEvents.ShowMessageEvent -= ShowMessage;
         }
 
-        if (FadeMessage == false && canvas.alpha > 0)
+        public void ToggleGameobject(GameObject _gameObject)
         {
-            canvas.alpha = canvas.alpha -= Time.deltaTime * MessageWaitTime;
+            _gameObject.SetActive(!_gameObject.activeSelf);
         }
-    }
 
-    public void CallStartGameEvent()
-    {
-        GameEvents.ResetGame();
-        Game.Manage.GameName = GameNameInput.text;
-    }
+        public void Update()
+        {
+            CanvasGroup canvas = MessageBoxGameObject.GetComponent<CanvasGroup>();
 
-    public void ShowMessage(string _message, float _time)
-    {
-        if (_time != 1.5f)
+            if (GameTimeClockText != null)
+            {
+                GameTimeClockText.text = "Time Remaining \n" + " Days " + Game.Manage.Timers.GameTimeLimit.Day + " Time: " + Game.Manage.Timers.GameTimeLimit.TimeString_H_MM;
+            }
+
+            if (FadeMessage && (int)canvas.alpha == 0)
+            {
+                canvas.alpha = canvas.alpha += Time.deltaTime * MessageWaitTime;
+            }
+
+            if (FadeMessage == false && canvas.alpha > 0)
+            {
+                canvas.alpha = canvas.alpha -= Time.deltaTime * MessageWaitTime;
+            }
+        }
+
+        public void CallStartGameEvent()
+        {
+            GameEvents.ResetGame();
+            Game.Manage.CurrentGameProject.Name = GameNameInput.text;
+        }
+
+        public void ShowMessage(string _message, float _time)
         {
             MessageWaitTime = _time;
+            MessageBoxText.text = _message;
+            StartCoroutine(IE_ShowMessageWait(_time));
         }
-        MessageBoxText.text = _message;
-        StartCoroutine(IE_ShowMessageWait(_time));
+
+        public IEnumerator IE_ShowMessageWait(float _time)
+        {
+            FadeMessage = true;
+            yield return new WaitForSeconds(_time);
+            FadeMessage = false;
+        }
+
+        public void ShowRewardAvert()
+        {
+            UnityAds.ShowRewardedAdvertisement();
+        }
+
+        public void SetGameState(int gamestateindex)
+        {
+            Game.Manage.CurrentGameState = (GameState)gamestateindex;
+        }
+
+        public void SetFeature(int featureindex)
+        {
+            Game.Manage.CurrentGameProject.Feature = (FeatureType)featureindex;
+        }
     }
 
-    public IEnumerator IE_ShowMessageWait(float _time)
-    {
-        FadeMessage = true;
-        yield return new WaitForSeconds(_time);
-        FadeMessage = false;
-    }
 }
